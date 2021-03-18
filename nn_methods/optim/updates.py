248fa7d4cc109e226d5829c2c6d4108e12bbc47b
Manos Kirtas <manolis.kirt@gmail.com>
2021-03-18 10:54:52 +0200

    Add NNSGD_Momentum

--- nn_methods/optim/updates.py
+++ nn_methods/optim/updates.py
@@ -6,6 +6,14 @@ class M_ABS(object):
     def __call__(self, p, grad, lr_in=1, lr_out=1):
         p.addcmul_(p.abs(), grad.mul(lr_in).tanh(), value=-lr_out)
 
+    @T.no_grad()
+    def tanh_part(self, grad, lr_in=1):
+        return grad.mul(lr_in).tanh()
+
+    @T.no_grad()
+    def update(self, p, grad, lr_out=1):
+        p.addcmul_(p.abs(), grad, value=-lr_out)
+
     def __repr__(self):
         return "M_ABS"
 
@@ -14,6 +22,14 @@ class M_SPOW(object):
     @T.no_grad()
     def __call__(self, p, grad, lr_in=1, lr_out=1):
         p.mul_(T.pow(2, grad.mul(lr_in).tanh().mul(lr_out).mul(-p.sign())))
+
+    @T.no_grad()
+    def tanh_part(self, grad, lr_in=1):
+        return grad.mul(lr_in).tanh()
+
+    @T.no_grad()
+    def update(self, p, grad, lr_out):
+        p.mul_(T.pow(2, grad.mul(lr_out).mul(-p.sign())))
 
     def __repr__(self):
         return "M_SPOW"
